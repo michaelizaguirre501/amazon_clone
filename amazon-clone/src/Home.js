927bc36354244d5049dbe5998f9aafe52c232a6f
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-09-22 16:15:34 -0400

    products hardcoded

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -13,50 +13,50 @@ function Home() {
       {/* product*/}
       <div className="homeRow">
         <Product
-          id="12345"
-          title="Example"
-          price="15.00"
-          rating={5}
-          image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
+          id="1"
+          title="Vitamix 5200 Blender Professional-Grade"
+          price="432.99"
+          rating={4}
+          image="https://m.media-amazon.com/images/I/71qy1IaCUqL._AC_SX679_.jpg"
         />
         <Product
-          id="12345"
-          title="Example"
+          id="2"
+          title="SteelSeries Arctis 9X Wireless Gaming Headset – Integrated Xbox Wireless + Bluetooth "
+          price="199.99"
+          rating={5}
+          image="https://m.media-amazon.com/images/I/81t7C7EiagL._AC_SX522_.jpg"
+        />
+      </div>
+      <div className="homeRow">
+        <Product
+          id="3"
+          title="Funko Pop! Star Wars: The Mandalorian - The Child, Grogu with Cookie"
+          price="8.79"
+          rating={5}
+          image="https://m.media-amazon.com/images/I/510GfbgM02L._AC_SX569_.jpg"
+        />
+        <Product
+          id="4"
+          title="Whiskey Glasses-Premium 11 OZ Scotch Glasses Set of 6 /Old Fashioned Whiskey Glasses"
           price="15.00"
+          rating={3}
+          image="https://m.media-amazon.com/images/I/81rX4xm2BBL._AC_SL1500_.jpg"
+        />
+        <Product
+          id="5"
+          title="Breville BNV250CRO Nespresso Vertuo, Espresso Machine, Black"
+          price="249.50"
           rating={5}
           image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
         />
       </div>
       <div className="homeRow">
         <Product
-          id="12345"
-          title="Example"
-          price="15.00"
-          rating={5}
-          image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
-        />
-        <Product
-          id="12345"
-          title="Example"
-          price="15.00"
-          rating={5}
-          image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
-        />
-        <Product
-          id="12345"
-          title="Example"
-          price="15.00"
-          rating={5}
-          image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
-        />
-      </div>
-      <div className="homeRow">
-        <Product
-          id="12345"
-          title="Example"
-          price="15.00"
-          rating={5}
-          image="https://m.media-amazon.com/images/I/71rhMv9KDYL._AC_SX679_.jpg"
+          id="6"
+          title="SAMSUNG 49-inch Odyssey G9 Gaming Monitor | QHD, 240hz, 1000R Curved, QLED, NVIDIA G-SYNC & FreeSync | LC49G95TSSNXZA Model"
+          price="1339.99"
+          rating={4}
+          image="https://m.media-amazon.com/images/I/61SQz8S+fEL._AC_SL1000_.jpg"
         />
       </div>
     </div>
